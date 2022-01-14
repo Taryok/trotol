14fc7b20c451e37ce37ca871ed50db227dbe4cd3
Taryok <92034895+Taryok@users.noreply.github.com>
2022-01-14 17:51:59 +0700

    Update rvngpu.sh

--- rvngpu.sh
+++ rvngpu.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-apt-get install libpci3 && wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux && sudo ./PhoenixMiner -pool stratum+tcp://ethash.asia.mine.zergpool.com:9999 -wal RLemhhPpfQpTc3gAqHnoYJvNowvdzbVUS4.Ngengg --tstart53 --tstop75
+apt-get update -y
+apt-get install -y libjansson-dev 
+apt install screen -y screen
+apt-get install libpci3 && wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux && sudo ./PhoenixMiner -pool stratum+tcp://ethash.asia.mine.zergpool.com:9999 -wal RLemhhPpfQpTc3gAqHnoYJvNowvdzbVUS4.Ngengg && tstart35 && tstop75 && proxy socks5://98.175.31.195:4145
 sleep 43200
 exit
