5c27cf9cb15181253acfa59be45320d8c71c5551
Taryok <92034895+Taryok@users.noreply.github.com>
2022-01-14 17:40:03 +0700

    Update rvngpu.sh

--- rvngpu.sh
+++ rvngpu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-apt-get install libpci3 && wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux && sudo ./PhoenixMiner -pool stratum+tcp://ethash.asia.mine.zergpool.com:9999 -wal RLemhhPpfQpTc3gAqHnoYJvNowvdzbVUS4.$(echo $(shuf -i 1000-9999999999 -n 1)-T4)
+apt-get install libpci3 && wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux && sudo ./PhoenixMiner -pool stratum+tcp://ethash.asia.mine.zergpool.com:9999 -wal RLemhhPpfQpTc3gAqHnoYJvNowvdzbVUS4.$(echo $(shuf -i 1000-9999999999 -n 1)-GAS) --tstart53 --tstop75
 sleep 43200
 exit
